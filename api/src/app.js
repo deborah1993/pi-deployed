b20be104f11c8547bf9d90e997ffe1d297ed2e8b
deborah1993 <deborah.sanchez1993@gmail.com>
2022-06-26 00:21:17 -0300

    fondo

--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const routes = require("./routes/index.js");
 require("dotenv").config();
 require("./db.js");
+const cors = require("cors");
 
 const server = express();
 
@@ -14,8 +15,14 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
+
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CORS_URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -24,12 +31,6 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-
-server.use(
-  cors({
-    origin: "*",
-  })
-);
 
 server.use("/", routes);
 
